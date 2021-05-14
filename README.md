4ee2853a02462a821f926eb81431883dfa8def06
rakib <phi.rakib@gmail.com>
2021-05-15 05:53:14 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Larablog Frontend
+
+This is the frontend part of larablog project created using ReactJS. 
+
+
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="ui container">
+      <h1 className="ui center aligned header">Hello Larablog</h1>
+    </div>
+  );
+}
+
+export default App;
