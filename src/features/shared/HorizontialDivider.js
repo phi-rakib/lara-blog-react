d210b1cead95d08ee954a7815375eddd54248ff3
rakib <phi.rakib@gmail.com>
2021-05-15 22:41:17 +0600

    created shared component horizontal divider which divides component by a single line

---
+++ src/features/shared/HorizontialDivider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HorizontialDivider({ children }) {
+  return (
+    <>
+      <h4 className="ui horizontal divider">{children}</h4>
+    </>
+  );
+}
+
+export default HorizontialDivider;
