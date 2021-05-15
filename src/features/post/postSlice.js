8bf1ec612a856c591196396f86f05164f9c6a040
rakib <phi.rakib@gmail.com>
2021-05-15 22:43:24 +0600

    added reducer logic to get all posts as well as single post

---
+++ src/features/post/postSlice.js
@@ -0,0 +1,56 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts?_limit=10"
+    );
+    return response.data;
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+export const fetchPost = createAsyncThunk("post/fetchPost", async (id) => {
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+const initialState = {
+  posts: [],
+  post: {},
+  status: "idle",
+  error: null,
+};
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.posts = action.payload;
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    },
+    [fetchPost.fulfilled]: (state, action) => {
+      state.post = action.payload;
+    },
+  },
+});
+
+export const { postReset } = postSlice.actions;
+
+export default postSlice.reducer;
