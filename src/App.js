2b9e5dd7f3d737082364378f1e791fa576c041b0
rakib <phi.rakib@gmail.com>
2021-05-15 22:37:56 +0600

    added menu component and route paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import PostListComponent from "./features/post/PostListComponent";
+import MenuComponent from "./features/menu/MenuComponent";
+import { Route, Switch } from "react-router";
+import PostPageComponent from "./features/post/PostPageComponent";
+
 function App() {
   return (
     <div className="ui container">
-      <h1 className="ui center aligned header">Hello Larablog</h1>
+      <MenuComponent />
+      <Switch>
+        <Route exact path="/" component={PostListComponent} />
+        <Route path="/posts/:id" component={PostPageComponent} />
+      </Switch>
     </div>
   );
 }
