7da1ac9e905de00229af0dc9cc9fe77cf017425a
rakib <phi.rakib@gmail.com>
2021-05-16 23:00:17 +0600

    modified menu style and fixed menu active problem by using NavLink instead of Link.

--- src/features/menu/MenuComponent.js
+++ src/features/menu/MenuComponent.js
@@ -1,20 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function MenuComponent() {
   return (
-    <div className="ui secondary  menu">
-      <Link to="/" className="active item">Home</Link>
-      <Link to="/messages" className="item">Messages</Link>
-      <Link to="/friends" className="item">Friends</Link>
-      <div className="right menu">
-        <div className="item">
-          <div className="ui icon input">
-            <input type="text" placeholder="Search..." />
-            <i className="search link icon"></i>
-          </div>
+    <div className="ui container">
+      <div className="ui large secondary inverted pointing menu">
+        <NavLink exact to="/" className="item">
+          Home
+        </NavLink>
+        <NavLink to="/post/add" className="item" activeClassName="active">
+          Add Post
+        </NavLink>
+        <NavLink to="/post/edit" className="item" activeClassName="active">
+          Company
+        </NavLink>
+        <NavLink to="/post/delete" className="item" activeClassName="active">
+          Careers
+        </NavLink>
+        <div className="right item">
+          <NavLink
+            to="/login"
+            className="ui inverted button"
+            activeClassName="active"
+          >
+            Log in
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="ui inverted button"
+            activeClassName="active"
+          >
+            Sign Up
+          </NavLink>
         </div>
-        <Link to="/logout" className="ui item">Logout</Link>
       </div>
     </div>
   );
