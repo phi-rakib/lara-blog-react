d5a5721a33f769f5eef15b7afd3b6b53a0350c3d
rakib <phi.rakib@gmail.com>
2021-05-16 23:14:49 +0600

    added site header, footer, changed css style and added two routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import PostListComponent from "./features/post/PostListComponent";
 import MenuComponent from "./features/menu/MenuComponent";
 import { Route, Switch } from "react-router";
 import PostPageComponent from "./features/post/PostPageComponent";
+import AddPostComponent from "./features/post/AddPostComponent";
+import FooterComponent from "./features/shared/FooterComponent";
+import HeaderComponent from "./features/shared/HeaderComponent";
+import LoginComponent from "./features/auth/LoginComponent";
 
 function App() {
   return (
-    <div className="ui container">
-      <MenuComponent />
-      <Switch>
-        <Route exact path="/" component={PostListComponent} />
-        <Route path="/posts/:id" component={PostPageComponent} />
-      </Switch>
-    </div>
+    <>
+      <div className="pusher">
+        <div className="ui inverted vertical masthead center aligned segment">
+          <MenuComponent />
+          <HeaderComponent />
+        </div>
+      </div>
+      <div className="pusher">
+        <div className="ui vertical stripe segment">
+          <Switch>
+            <Route exact path="/" component={PostListComponent} />
+            <Route path="/posts/:id" component={PostPageComponent} />
+            <Route path="/post/add" component={AddPostComponent} />
+            <Route path="/login" component={LoginComponent} />
+          </Switch>
+        </div>
+      </div>
+      <div className="ui divider"></div>
+      <FooterComponent />
+    </>
   );
 }
 
