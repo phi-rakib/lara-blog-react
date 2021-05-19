d90452933e59796d4538e8149bc6eddceed5b80c
rakib <phi.rakib@gmail.com>
2021-05-19 22:53:13 +0600

    conditionaly added devTools when it's not in production, added empty preloadedState and authReducer.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/post/postSlice";
 import commentReducer from "../features/comment/commentSlice";
+import authReducer from "../features/auth/authSlice";
+
+const reducer = {
+  posts: postReducer,
+  comments: commentReducer,
+  auth: authReducer,
+};
+const preloadedState = {};
 
 const store = configureStore({
-  reducer: {
-    posts: postReducer,
-    comments: commentReducer,
-  },
+  reducer,
+  devTools: process.env.NODE_ENV !== "production",
+  preloadedState,
 });
 
 export default store;
