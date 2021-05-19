f222a134832ae764c7f6ba0b88fe56dc9a32d45c
rakib <phi.rakib@gmail.com>
2021-05-19 23:31:02 +0600

    moved data fetching logic to slice, added state resetting functionality, checking status before showing single post component.

--- src/features/post/PostPageComponent.js
+++ src/features/post/PostPageComponent.js
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import CommentListComponent from "../comment/CommentListComponent";
 import TagComponent from "../shared/TagComponent";
 import SinglePostComponent from "./SinglePostComponent";
 import AuthorShortBioComponent from "./AuthorShortBioComponent";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPost, postReset, postStatus } from "./postSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
+import LoaderComponent from "./../shared/LoaderComponent";
 
 function PostPageComponent({ match }) {
   const { id } = match.params;
 
-  const initialState = { title: "", body: "" };
-  const [post, setPost] = useState(initialState);
+  const dispatch = useDispatch();
 
-  const getPost = async (id) => {
-    try {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts/${id}`
-      );
-      setPost(response.data);
-    } catch (error) {
-      throw Error(error);
-    }
-  };
+  const status = useSelector(postStatus);
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    const getPost = async (id) => {
+      try {
+        const result = await dispatch(fetchPost(id));
+        unwrapResult(result);
+      } catch (error) {
+        console.error("Failed to fetch post ", error);
+      }
+    };
+
     getPost(id);
-  }, [id]);
+
+    return () => {
+      dispatch(postReset());
+    };
+  }, [id, dispatch]);
 
   return (
     <div className="ui text container">
-      <SinglePostComponent post={post} />
+      {status === "succeeded" ? <SinglePostComponent /> : <LoaderComponent />}
       <div className="ui basic segment">
         <TagComponent>HTML</TagComponent>
         <TagComponent>CSS</TagComponent>
